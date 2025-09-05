9a0b06b0b917cadd446966900fc58a58f68ef677
kettumigu <mikko.keranen@gmail.com>
2025-09-05 13:45:01 +0300

    app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ def arvioi_riski():
     mPFS, mOS = arvioi_mPFS_mOS(deleetio_17p)
     if mPFS is not None:
         korkea_riski = True
-        st.warning("Korkea riski: 17p-deleetio ≥ 20 %, jos potilaalla on samanaikainen TP53 mutaatio, ennuste on estimaattia huonompi.")
+        st.warning("Korkea riski: 17p-deleetio ≥ 20 %. Jos potilaalla on samanaikainen TP53 mutaatio, ennuste on estimaattia huonompi.")
         st.write(f"**Arvioitu mPFS:** {mPFS:.1f} kk")
         st.write(f"**Arvioitu mOS:** {mOS:.1f} kk")
 
@@ -45,16 +45,22 @@ def arvioi_riski():
         tp53 = st.radio("Onko potilaalla TP53-mutaatio/deleetio?", ["Ei", "Kyllä"], horizontal=True)
         if tp53 == "Kyllä":
             korkea_riski = True
-            st.warning("Korkea riski: TP53-muutos")
+            st.warning(
+            "Korkea riski: TP53-muutos\n\n"
+            "Jos deleetio, mOS n. 75 kk ja jos mutaatio, mOS n. 54-60 kk)"
+        )
+
 
     if not korkea_riski:
         t_1416 = st.radio("Onko potilaalla t(14;16)?", [
-            "Ei tai ainoana muutoksena",
-            "Kyllä, yhdessä del(1p32)/1q+ kanssa"
+            "Ei tai ainoana muutoksena (harvinainen)",
+            "Kyllä, yhdessä del(1p32) tai 1q+ kanssa"
         ])
         if t_1416.startswith("Kyllä"):
             korkea_riski = True
-            st.warning("Korkea riski: t(14;16) + del(1p32)/1q+")
+            st.warning("Korkea riski: t(14;16) + del(1p32) tai 1q+\n\n"
+            "Jos WT tai ainoana muutoksena, mOS 92 kk, jos del(1p32) tai 1q+ kanssa, mOS 61 kk"
+        )
 
     if not korkea_riski:
         t_4414 = st.radio("Onko potilaalla t(4;14)?", [
@@ -63,8 +69,9 @@ def arvioi_riski():
         ])
         if t_4414.startswith("Kyllä"):
             korkea_riski = True
-            st.warning("Korkea riski: t(4;14) + del(1p32)/1q+")
-
+            st.warning("Korkea riski: t(14;16) + del(1p32) tai 1q+\n\n"
+            "Jos ainoana muutoksena, mOS ei saavutettu, jos del(1p32) tai 1q+ kanssa, mOS 70 kk"
+        )
     if not korkea_riski:
         del_1p32_1q = st.radio(
             "Onko bialleelinen del(1p32) tai del(1p32) ja 1q+?",
@@ -72,7 +79,9 @@ def arvioi_riski():
         )
         if del_1p32_1q == "Kyllä":
             korkea_riski = True
-            st.warning("Korkea riski: bialleelinen del(1p32) tai del(1p32) ja 1q+")
+            st.warning("Korkea riski: bialleelinen del(1p32) tai del(1p32) ja 1q+\n\n"
+            "Jos bialleelinen del(1p32) tai del(1p32) ja 1q+, mOS 39 kk"
+        )
 
     if not korkea_riski:
         b2m_krea = st.radio(
@@ -81,7 +90,9 @@ def arvioi_riski():
         )
         if b2m_krea == "Kyllä":
             korkea_riski = True
-            st.warning("Korkea riski: B2M/krea-yhdistelmä")
+            st.warning("Korkea riski: B2M/krea-yhdistelmä\n\n"
+            "Jos B2M >5.5 mg/dL, ainoa muutos tai yhdessä korkean riskin geneettisten muutosten kanssa, mOS 63-75 kk"
+        )
 
     # Lopputulos aina näkyviin
     st.markdown("---")
@@ -89,8 +100,11 @@ def arvioi_riski():
         st.success("➡️ Riskiluokka: **KORKEA**")
     else:
         st.info("➡️ Riskiluokka: **MATALA**")
+        st.write(
+            "- Jos 1q+ ainoana muutoksena, mOS 96 kk\n"
+            "- Jos monoalleelinen del(1p32), mOS 77 kk\n"
+            "- Jos ei mitään em., ennuste paras, eikä mOS ole 9v seurannassa saavutettu."
+    )
 
 if __name__ == "__main__":
     arvioi_riski()
-
-
